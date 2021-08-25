9011b2f202ef7c66d4b994f5bae36b4de446e274
Jef Mitchell <jef@project87.net>
2021-08-24 21:06:27 -0400

    Update Javascript - (index.js) Removed questions and created init(). - (employee.js) Created get functions. - (engineer.js) Commented out functions. - (intern.js) Commented out functions. - (manager.js) Created questions.

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const emailValidator = require("email-validator");
 
-inquirer
-  .prompt([
-    {
-      name: "name",
-      message: `What is the ${role}'s name?`,
-    },
-    {
-      name: "id",
-      message: `What is the ${role}'s id?`,
-    },
-    {
-      name: "email",
-      message: `What is the ${role}'s email?`,
-      validate(input) {
-        if (emailValidator.validate(input)) {
-          return true;
-        } else {
-          return "Please enter a valid email address.";
-        }
-      },
-    },
-    {
-      name: "officeNumber",
-      message: `What is the ${role}'s office number?`,
-    },
-    {
-      type: "list",
-      name: "addRole",
-      message: "Which team member would you like to add?",
-      choices: [
-        "Engineer",
-        "Intern",
-        "I don't want to add anymore team members.",
-      ],
-    },
-  ])
-  .then((answers) => {
-    console.log(answers);
+const Manager = require("./lib/manager");
+
+let questions = [];
+
+const manager = new Manager();
+
+function init() {
+  inquirer.prompt(manager.getRole(questions)).then((answers) => {
+    console.log({ manager });
+    console.log({ answers });
   });
+};
+
+init()
