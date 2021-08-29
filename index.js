50f1661fe7dd4f53742d6e7bdb3ea864eaf2d9bf
Jef Mitchell <jef@project87.net>
2021-08-29 18:36:42 -0400

    Update Javascript - (index.js) Moved question objects here from class files. - (index.js) Built createTeam(). - (index.js) Added card creation via buildCard(). - (index.js) Repalced storeAnswers() with buildHTML().

--- index.js
+++ index.js
@@ -1,93 +1,177 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const emailValidator = require("email-validator");
 
+const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
-const Manager = require("./lib/manager");
 
-const manager = new Manager();
-const engineer = new Engineer();
-const intern = new Intern();
+let role = "Manager";
+let teamArray = [];
 
-let questions = [];
-let employee = "Manager";
-let team = [];
+let questionsRole = [
+  {
+    name: "officeNumber",
+    message: `What is the Manager's office number?`,
+  },
+  {
+    name: "github",
+    message: "What is the Engineer's github?",
+  },
+  {
+    name: "school",
+    message: "What school does the Intern attend?",
+  },
+];
 
-let managerArray = [];
-let engineerArray = [];
-let internArray = [];
+let addRole = {
+  type: "list",
+  name: "addRole",
+  message: "Which team member would you like to add?",
+  choices: ["Engineer", "Intern", "I don't want to add anymore team members."],
+};
 
-function init() {
-  switch (employee) {
-    case "Manager":
-      employeeQuestions = manager.getQuestions(questions);
-      employeeRole = manager.getRole();
-      break;
-    case "Engineer":
-      employeeQuestions = engineer.getQuestions(questions);
-      employeeRole = engineer.getRole();
-      break;
-    case "Intern":
-      employeeQuestions = intern.getQuestions(questions);
-      employeeRole = intern.getRole();
-      break;
-    default:
-      break;
-  }
-
-  inquirer
-    .prompt(employeeQuestions)
-    .then((answers) => {
-      let employeeWithRole = { ...answers, employeeRole };
-      return employeeWithRole;
-    })
-    .then((employeeWithRole) => {
-      switch (employeeWithRole.addRole) {
-        case "Engineer":
-          storeAnswers(employeeWithRole);
-          employee = "Engineer";
-          init();
-          break;
-        case "Intern":
-          storeAnswers(employeeWithRole);
-          employee = "Intern";
-          init();
-          break;
-        case "I don't want to add anymore team members.":
-          // buildHTML();
-          storeAnswers(employeeWithRole);
-          break;
-      }
-    });
+function questionsBase(e) {
+  return (questions = [
+    {
+      name: "name",
+      message: `What is the ${e}'s name?`,
+    },
+    {
+      name: "id",
+      message: `What is the ${e}'s id?`,
+    },
+    {
+      name: "email",
+      message: `What is the ${e}'s email?`,
+      validate(input) {
+        if (emailValidator.validate(input)) {
+          return true;
+        } else {
+          return "Please enter a valid email address.";
+        }
+      },
+    },
+  ]);
 }
 
-function storeAnswers(e) {
-  switch (employee) {
+function createTeam() {
+  let updatedQuestions = [];
+  switch (role) {
     case "Manager":
-      console.log({ employee });
-      managerArray.push(e);
-      console.log({ managerArray });
+      managerQuestions = questionsBase(role);
+      updatedQuestions = [...managerQuestions, questionsRole[0], addRole];
+      inquirer.prompt(updatedQuestions).then((answers) => {
+        let manager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        role = answers.addRole;
+        teamArray.push(buildCard(manager));
+        console.log({ teamArray });
+        createTeam();
+      });
       break;
     case "Engineer":
-      console.log({ employee });
-      engineerArray.push(e);
-      console.log({ engineerArray });
+      engineerQuestions = questionsBase(role);
+      updatedQuestions = [...engineerQuestions, questionsRole[1], addRole];
+      inquirer.prompt(updatedQuestions).then((answers) => {
+        let engineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        role = answers.addRole;
+        teamArray.push(buildCard(engineer));
+        console.log({ teamArray });
+        createTeam();
+      });
       break;
     case "Intern":
-      console.log({ employee });
-      internArray.push(e);
-      console.log({ internArray });
+      internQuestions = questionsBase(role);
+      updatedQuestions = [...internQuestions, questionsRole[2], addRole];
+      inquirer.prompt(updatedQuestions).then((answers) => {
+        let intern = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.school
+        );
+        role = answers.addRole;
+        teamArray.push(buildCard(intern));
+        console.log({ teamArray });
+        createTeam();
+      });
       break;
-    default:
+    case "I don't want to add anymore team members.":
+      buildHTML();
       break;
   }
+}
+
+function buildCard(a) {
+  let roleTitle = "";
+  let roleSpecialIcon = "";
+  let roleSpecialTitle = "";
+  let roleSpecial = "";
+
+  if (a instanceof Manager) {
+    roleTitle = a.getRole();
+    roleSpecialIcon = "fas fa-mug-hot";
+    roleSpecialTitle = "Office Number: ";
+    roleSpecial = a.getOfficeNumber();
+  } else if (a instanceof Engineer) {
+    roleTitle = a.getRole();
+    roleSpecialIcon = "fas fa-glasses";
+    roleSpecialTitle = "GitHub: ";
+    let github = a.getGithub();
+    roleSpecial = `<a href="https://github.com/${github}">${github}</a>`
+  } else if (a instanceof Intern) {
+    roleTitle = a.getRole();
+    roleSpecialIcon = "fas fa-user-graduate";
+    roleSpecialTitle = "School: ";
+    roleSpecial = a.getSchool();
+  } else {
+    return;
+  }
+
+  let card = `
+      <div class="card">
+        <div class="card-header">
+          <h4 class="eName">${a.name}</h4>
+          <h4 class="eRole"><i class="${roleSpecialIcon}"></i> ${roleTitle}</h4>
+        </div>
+        <ul class="list-group">
+          <li class="list-group-item eId">ID: ${a.id}</li>
+          <li class="list-group-item eEmail">Email: <a href="mailto:${a.email}">${a.email}</a></li>
+          <li class="list-group-item eUnique">${roleSpecialTitle}${roleSpecial}</li>
+        </ul>
+      </div>`;
+
+  return card;
 }
 
 function buildHTML() {
   fs.readFile("./src/index.html", (err, data) => {
-    if (err) throw err;
-    console.log(data);
+    if (err) {
+      console.log(err);
+    } else {
+      let cleanTeam = teamArray.toString().replace(",", "");
+      let result = data.toString().replace("<img>", cleanTeam);
+      fs.writeFile("./dist/index.html", result, "utf8", function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
   });
+}
+
+function init() {
+  createTeam();
 }
 
 init();
